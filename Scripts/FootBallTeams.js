417822bfd0d36a3d3722f2ddc8937d9ee999a8ee
phantasiaphillips <114950729+phantasiaphillips@users.noreply.github.com>
2022-11-03 10:00:12 -0400

    Foundation

---
+++ Scripts/FootBallTeams.js
@@ -0,0 +1,6 @@
+let teams = [
+    {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
+    {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
+    {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
+    {code:"KAN", name:"Kansas City Chiefs",
+    plays:"Kansas City, MO"},
