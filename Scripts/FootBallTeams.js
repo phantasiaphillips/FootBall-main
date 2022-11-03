187df664cddd43adabc3be4b4dbe0b3d6891b720
phantasiaphillips <114950729+phantasiaphillips@users.noreply.github.com>
2022-11-03 11:17:37 -0400

    funct dropdown

--- Scripts/FootBallTeams.js
+++ Scripts/FootBallTeams.js
@@ -1,7 +1,55 @@
-let teams = [
+"use strict";
+
+window.onload - init;
+
+const teams = [
     {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
     {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
     {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
     {code:"KAN", name:"Kansas City Chiefs",
     plays:"Kansas City, MO"},
 ]
+
+function init()
+{
+    console.log("FootBallTeams.js");
+    fillDropdown();
+    const selectBtn = document.getElementById("selectBtn");
+    selectBtn.onclick = selectBtnOnClick;
+//document.getElementaryById("selectBtn") - selectBtnOnClick;
+
+}
+
+function fillDropdown(){
+    const teamSelect = document.getElementById(teamSelect);
+    let teamsLength = teams.length;
+    for(let i = 0 ; i <teamsLength ; i++){
+        let newOption = document.createElement("option");
+        newOption.value = teams[i].code;
+        newOption.textContent = teams[i].name;
+
+        teamSelect.appendChild(newOption)
+    }
+}
+
+function selectBtnOnClick() {
+    const teamSelect = document.getElementById("teamSelect");
+    let selectedTeamCode = teamSelect.value;
+    let selectedTeam = getTeamFromCode(teams, selectedTeamCode);
+
+    let message = "You selected the ${selectedTeam.name} (${selectedTeam.code}) who play in ${selectedTeam.plays}.
+    const teamInfo = document.getElementById(teamInfo);
+    teamInfo.innerHTML = message;
+}
+
+
+
+
+
+
+
+
+
+
+
+
